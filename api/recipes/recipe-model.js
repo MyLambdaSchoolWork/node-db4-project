7657cfd19fee7c5b7dd6288ce53c09736f2cc794
Matthew Ellis <mbellistx@gmail.com>
2021-04-16 02:27:05 -0500

    I lied and did it anyway. It is not a good way. Please ask for help tomorrow

--- api/recipes/recipe-model.js
+++ api/recipes/recipe-model.js
@@ -33,12 +33,84 @@ module.exports = {
 }
 
 async function getRecipeById(id){
-  const recipeTable = await db('recipies as r')
-    .join('steps as st', 'st.recipe_id', 'r.recipe_id')
-    .leftJoin('step_ingredients as st_i', 'st.step_id', 'st_i.step_id')
-    .leftJoin('ingredients as i', 'i.ingredient_id', 'st_i.ingredient_id')
-    .where('r.recipe_id', id)
-    .orderBy('st.step_num')
+  let recipeTable;
+  try {
+    recipeTable = await db('recipes as r')
+      .join('steps as st', 'st.recipe_id', 'r.recipe_id')
+      .leftJoin('step_ingredients as st_i', 'st.step_id', 'st_i.step_id')
+      .leftJoin('ingredients as i', 'i.ingredient_id', 'st_i.ingredient_id')
+      .select('r.*', 'st.step_num', 'st.step_instructions as instructions', 'i.ingredient_name', 'st_i.quantity')
+      .where('r.recipe_id', id)
+      .orderBy('st.step_num')
+  } catch(err) {
+    console.log(err, err.message)
+    return
+  }
 
-  
+  // console.log(recipeTable)
+
+  const recipeJson = {
+    id,
+    recipe_name: recipeTable[0].recipe_name,
+    steps: []
+  }
+
+  const steps = []
+  let lastStep = 0
+  recipeTable.forEach( (part, i) => {
+    const stepNum = part.step_num
+    if(lastStep === stepNum){
+      return
+    } else {
+      lastStep = stepNum
+    }
+    const newStep = {
+      step_num: stepNum,
+      instructions: part.instructions
+    }
+
+    if(part.ingredient_name){
+      const ingredients = []
+      for(let j = i; (j < recipeTable.length && recipeTable[j].step_num === stepNum); j++){
+        ingredients.push({
+          ingredient_name: recipeTable[j].ingredient_name,
+          quantity: recipeTable[j].quantity
+        })
+      }
+      steps.push({
+        ...newStep,
+        ingredients
+      })
+      // return {
+      //   ...newStep,
+      //   ingredients
+      // }
+    } else {
+      steps.push({...newStep})
+      // return newStep
+    }    
+  })
+
+  recipeJson.steps = steps
+  return recipeJson
 }
+
+getRecipeById(2)
+  .then( res => {
+    console.log(res)
+    console.log(res.steps[0].ingredients)
+    console.log('\n')
+  })
+  .catch( err => {
+    console.log(err, err.message)
+  })
+
+
+getRecipeById(1)
+  .then( res => {
+    console.log(res)
+    console.log(res.steps[2].ingredients)
+  })
+  .catch( err => {
+    console.log(err, err.message)
+  })
